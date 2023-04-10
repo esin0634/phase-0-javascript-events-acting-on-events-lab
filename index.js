b864ca310496a2fd6f4d4be1d420c6ce9a8ccf76
Esin <nbusra.esin@gmail.com>
2023-04-10 18:30:11 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Your code here
+const dodger = document.querySelector("#dodger")
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
+
+    if(left < 360){
+        dodger.style.left = left+10 +"px";
+    }
+}
+
+document.addEventListener("keydown", function(event){
+    if (event.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+})
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
+
+    if(left > 0){
+        dodger.style.left = left-10 +"px";
+    }
+}
+
+document.addEventListener("keydown", function(event){
+    if (event.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }
+})
+function moveDodgerUp(){
+    const bottomNumbers = dodger.style.bottom.replace("px", "")
+    const bottom = parseInt(bottomNumbers, 10)
+
+    if(bottom < 360){
+        dodger.style.bottom = bottom+10 +"px";
+    }
+}
+
+document.addEventListener("keydown", function(event){
+    if (event.key === "ArrowUp"){
+        moveDodgerUp();
+    }
+})
+function moveDodgerDown(){
+    const bottomNumbers = dodger.style.bottom.replace("px", "")
+    const bottom = parseInt(bottomNumbers, 10)
+
+    if(bottom > 0){
+        dodger.style.bottom = bottom-10 +"px";
+    }
+}
+
+document.addEventListener("keydown", function(event){
+    if (event.key === "ArrowDown"){
+        moveDodgerDown();
+    }
+})
